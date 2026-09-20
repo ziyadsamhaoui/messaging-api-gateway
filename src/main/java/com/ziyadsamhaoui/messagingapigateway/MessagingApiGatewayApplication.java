56a0c6276b23b7cf9b80b25cef86d61c6a4043be
Ziyad SAMHAOUI <ziyad.samhaoui@usms.ac.ma>
2026-09-20 11:45:06 +0000

    initial commit

---
+++ src/main/java/com/ziyadsamhaoui/messagingapigateway/MessagingApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.ziyadsamhaoui.messagingapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessagingApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MessagingApiGatewayApplication.class, args);
+    }
+
+}
